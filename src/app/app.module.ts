5944b2efe6a6e24d7ae328faa7173c17c9e128d6
gustll <gal.1999k@gmail.com>
2025-03-28 14:15:37 +0100

    init: initial project setup

---
+++ src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+    declarations: [AppComponent],
+    bootstrap: [AppComponent],
+    imports: [AppRoutingModule, BrowserAnimationsModule, BrowserModule],
+})
+export class AppModule {}
